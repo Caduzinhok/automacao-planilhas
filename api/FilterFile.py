d3603ae3959970ce07e333a7c70b8d32e72c88a3
Caduzinhok <carlos_fire1@outlook.com>
2024-08-15 20:23:00 -0300

    feat: optimize execution time

--- api/FilterFile.py
+++ api/FilterFile.py
@@ -2,69 +2,71 @@ import pandas as pd
 from io import BytesIO
 
 def main(file_initial_obj, file_final_obj, name_file):
-    ### Validações para evitar erros de formato.
-    if('.' in name_file):
-        name_file = str(name_file).replace(".", '')
-        
-    if('csv' in name_file):
-        name_file = str(name_file).replace(".csv", '')
-        
+    # Validações para o nome do arquivo
+    name_file = name_file.split('.')[0].replace(".csv", '')
+    
     print("Lendo dados do Arquivo CSV...")
 
     try:
-        ### Ler arquivos como CSV
+        # Ler arquivos como CSV
         base_inicial = pd.read_csv(file_initial_obj, sep=";", encoding='utf-8')
-        base_final =  pd.read_csv(file_final_obj, sep=";", encoding='utf-8') 
-        base_final['Status'] = ''
+        base_final = pd.read_csv(file_final_obj, sep=";", encoding='utf-8')
         base_inicial['Status'] = ''
-    except:
-        print("Não foi possível ler os arquivos do formato CSV, por favor verifique o formato ou o caminho...")
+        base_final['Status'] = ''
+    except Exception as e:
+        print(f"Erro ao ler os arquivos CSV: {e}")
+        return None
+
     print("Filtrando dados do arquivo CSV...")
 
     try:
-        ### Verificando items que estavam na base 1 (Ativos e Suspensos)
-        for index, item in base_inicial.iterrows():
-            user_mail = str(base_inicial.iloc[index]['username']) ### Obter valor da base na coluna 1 = username
-            curso = str(base_inicial.iloc[index]['course1']) ### Obter valor da base na coluna 1 = username
+        # Verificando items que estavam na base 1 (Ativos e Suspensos)
+        for index, row in base_inicial.iterrows():
+            user_mail = row['username']
+            curso = row['course1']
 
-            base_filtrada = base_final[base_final['username'] == user_mail]
-            base_filtrada = base_filtrada[base_filtrada['course1'] == curso]
+            if user_mail == "#N/D":
+                base_inicial.at[index, 'Status'] = 'suspended'
+                continue
 
-            if(len(base_filtrada) >= 1 and (user_mail != "#N/D")): ### Se encontrou o usuário e o usuário não é nulo ou undefined
-                base_inicial.at[index, 'Status'] = 'active'                
+            base_filtrada = base_final[(base_final['username'] == user_mail) & (base_final['course1'] == curso)]
+
+            if not base_filtrada.empty:
+                base_inicial.at[index, 'Status'] = 'active'
             else:
                 base_inicial.at[index, 'Status'] = 'suspended'
-    except:
-        print("Não foi possível cruzar as informações dos arquivos, verifique se os nomes das colunas estão como 'username' e 'course1'")
+    except KeyError as e:
+        print(f"Erro nas colunas: {e}")
+        return None
+    except Exception as e:
+        print(f"Erro ao cruzar informações: {e}")
+        return None
 
-    print('Juntando bases e salvando no Downloads...')
+    print("Juntando bases e salvando no Downloads...")
+    
     try:
-        base_completa = base_inicial.merge(base_final, on=["username", 'course1'], how='outer')
-    except:
-        print("Não foi possível juntar ambas as bases, verifique os nomes das colunas ou quantidade de colunas.")
-    
-    ### Organizar colunas duplicadas
-    base_completa = base_completa.drop(columns=['Status_y'])
-    base_completa = base_completa.rename(columns={"Status_x": "Status"})
-    
-    base_completa['enrolstatus1'] = ''
-    
-    base_completa.loc[base_completa['Status'].isnull(),'Status'] = 'new' # Aqueles que não foram encontrados, setar como new
-    
-    ### Criar coluna para o moodle
-    base_completa.loc[base_completa['Status'] == 'suspended','enrolstatus1'] = '1' # Coluna enrolstatus1
-    base_completa.loc[base_completa['Status'] == 'new','enrolstatus1'] = '0' # Coluna enrolstatus1
-    base_completa.loc[base_completa['Status'] == 'active','enrolstatus1'] = '0' # Coluna enrolstatus1
-    base_completa.loc[base_completa['Status'].isnull(),'enrolstatus1'] = '0' # Coluna enrolstatus1
+        # Juntar as bases
+        base_completa = pd.merge(base_inicial, base_final, on=["username", "course1"], how='outer', suffixes=('_inicial', '_final'))
+        base_completa.drop(columns=['Status_final'], inplace=True)
+        base_completa.rename(columns={"Status_inicial": "Status"}, inplace=True)
+        base_completa['enrolstatus1'] = ''
 
-    ## Salvar Arquivo na pasta de downloads
+        # Definir valores na coluna enrolstatus1
+        base_completa.loc[base_completa['Status'] == 'suspended', 'enrolstatus1'] = '1'
+        base_completa.loc[base_completa['Status'].isnull(), 'Status'] = 'new'
+        base_completa.loc[base_completa['Status'].isin(['new', 'active']), 'enrolstatus1'] = '0'
+    except Exception as e:
+        print(f"Erro ao juntar as bases: {e}")
+        return None
+
+    # Salvar o resultado em um objeto BytesIO
     try:
-        # Salva o resultado em um objeto BytesIO
         output = BytesIO()
         base_completa.to_csv(output, sep=";", encoding='utf-8-sig', index=False)
         output.seek(0)
-    except:
-        print("Não foi possível salvar base Planilhas de Ofertas no caminho")
+    except Exception as e:
+        print(f"Erro ao salvar o arquivo: {e}")
+        return None
+
     print("Dados salvos com sucesso na pasta!!!")
-    
     return output
